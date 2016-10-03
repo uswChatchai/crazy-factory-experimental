a6a13148704eba9b9a3d5886fb07a1f2bf10c0e3
Mr.Chatchai Ussawinkowit <usw.chatchai@gmail.com>
2016-10-04 02:15:26 +0700

    - Fixed wrong php syntax.

--- 01-1_optimize-sql/index.php
+++ 01-1_optimize-sql/index.php
@@ -1,7 +1,7 @@
 <?php
 
 // ####### Question 1: Optimize SQL for performance. ###########
-$con=mysqli_connect("localhost","my_user","my_password","my_db");
+$con=mysqli_connect("localhost:3306","root","12345","exp");
 // Check connection
 if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
@@ -10,18 +10,24 @@ if (mysqli_connect_errno()) {
 //queries
 $result = mysql_query('SELECT article_id, quantity FROM quality_control_articles WHERE qc_id=720');
 
-    while ( $row =  mysql_fetch_array ( $result )) {
-        mysql_query('
-            INSERT INTO stock_ledger SET article_id="' . addslashes($row['article_id']) . '"
-            bin="quality_control",
-            quantity = "' . addslashes($row['quantity']) . '"
-        ');
-        mysql_query('
-            INSERT INTO stock_ledger SET
-            article_id="' . addslashes($row['article_id']) . '"  bin="main_bin",
-            quantity = "' . addslashes($row['quantity']) . '"
-        ');
+//queries
+$result = mysqli_query($con,'SELECT article_id, quantity FROM quality_control_articles WHERE qc_id = 720');
+
+while ( $row = mysqli_fetch_array($result) ) {
+  mysqli_query($con,'
+    INSERT INTO stock_ledger SET
+    article_id = "' . addslashes($row['article_id']) . '",
+    bin = "quality_control",
+    quantity = "' . addslashes($row['quantity']) . '"
+  ');
+  mysqli_query($con,'
+    INSERT INTO stock_ledger SET
+    article_id="' . addslashes($row['article_id']) . '",
+    bin = "main_bin",
+    quantity = "' . addslashes(-$row['quantity']) . '"
+  ');
 }
+echo "The queries process is successful.";
 
 //If have data print out the result
 if ($result) {
